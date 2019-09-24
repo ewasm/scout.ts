4eb46c51cef70a21e0dd4acef1d7368aa1733804
Sina Mahmoodi <itz.s1na@gmail.com>
2019-09-24 16:35:28 +0200

    Add debug print imports

--- src/lib.ts
+++ src/lib.ts
@@ -58,6 +58,13 @@ export const getImports = (env: EnvData) => {
       debug_endTimer: () => console.log('end timer'),
       abort: () => { throw ('Wasm aborted') }
     },
+    debug: {
+      debug_print32: (value: number) => console.log('debug_print32', value),
+      debug_printMem: (ptr: number, length: number) => console.log('debug_printMem: ', ptr, length, memget(mem, ptr, length)),
+      debug_printMemHex: (ptr: number, length: number) => {
+        console.log('debug_printMemHex: ', ptr, length, memget(mem, ptr, length).toString('hex'))
+      }
+    },
     bignum: {
       add256: (aOffset: number, bOffset: number, cOffset: number) => {
         const a = new BN(memget(mem, aOffset, 32))
